a315b6ec84de916f46596eb78406b227fb3e193e
zk555 <602281555@qq.com>
2019-04-23 15:52:55 +0800

    引入AOP 切面功能，使用反射拿到@log注解标注的Method信息

---
+++ src/main/java/com/gy/struggle/common/utils/HttpContextUtils.java
@@ -0,0 +1,16 @@
+package com.gy.struggle.common.utils;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpContextUtils {
+	/**
+	 * 获取HttpServletRequest对象
+	 * @return
+	 */
+	public static HttpServletRequest getHttpServletRequest() {
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+	}
+}
