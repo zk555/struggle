f495f4815a4a5aafd5a84733c024bafde6218f34
zk555 <602281555@qq.com>
2019-05-07 17:55:36 +0800

    文件菜单完善

--- src/main/java/com/gy/struggle/common/service/FileService.java
+++ src/main/java/com/gy/struggle/common/service/FileService.java
@@ -13,8 +13,20 @@ import java.util.Map;
  * @date 2017-09-19 16:02:20
  */
 public interface FileService {
-	
+
 	FileDO get(Long id);
+
+	List<FileDO> list(Map<String, Object> map);
+
+	int count(Map<String, Object> map);
+
+	int save(FileDO sysFile);
+
+	int update(FileDO sysFile);
+
+	int remove(Long id);
+
+	int batchRemove(Long[] ids);
 
 	/**
 	 * 判断一个文件是否存在
--- src/main/java/com/gy/struggle/common/service/impl/FileServiceImpl.java
+++ src/main/java/com/gy/struggle/common/service/impl/FileServiceImpl.java
@@ -8,22 +8,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.util.StringUtils;
 
 
 @Service
 public class FileServiceImpl implements FileService {
+
 	@Autowired
 	private FileMapper sysFileMapper;
 
 	@Autowired
 	private StruggleConfig struggleConfig;
+
 	@Override
 	public FileDO get(Long id){
 		return sysFileMapper.get(id);
 	}
 
+	@Override
+	public List<FileDO> list(Map<String, Object> map){
+		return sysFileMapper.list(map);
+	}
+
+	@Override
+	public int count(Map<String, Object> map){
+		return sysFileMapper.count(map);
+	}
+
+	@Override
+	public int save(FileDO sysFile){
+		return sysFileMapper.save(sysFile);
+	}
+
+	@Override
+	public int update(FileDO sysFile){
+		return sysFileMapper.update(sysFile);
+	}
+
+	@Override
+	public int remove(Long id){
+		return sysFileMapper.remove(id);
+	}
+
+	@Override
+	public int batchRemove(Long[] ids){
+		return sysFileMapper.batchRemove(ids);
+	}
 
 	@Override
 	public Boolean isExist(String url) {
